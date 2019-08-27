2eaffebcc42661c60d3cd937f673bf2cae3c1160
Phil Sheard <phil@philipsheard.co.uk>
2019-08-27 10:13:28 +0100

    Change to bio

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ module.exports = {
   author: {
     name: 'Phil Sheard',
     photo: '/photo.jpg',
-    bio: 'Things that interest me.\n\nWork: Special Projects Director @ Battenhall, London. Looking after products, innovation and client strategy.',
+    bio: 'Things that interest me.\n\nWork: Special Projects Director @ Battenhall, London. Looking after products, innovation and strategy.',
     contacts: {
       email: '',
       facebook: '#',
